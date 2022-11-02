ff71a690f3b8e0cb66ea47646382909a00bd7141
@ahkCodeder <112486735+ahkCodeder@users.noreply.github.com>
2022-11-02 21:18:10 +0100

    Update main.py
    move amount added

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ YOU NEED TO HAVE DOPE SHEET OPEN
 # THIS SETS THE GAP BETWEEN KEY FRAMES FOR SELECTED OBJECT 0 WILL SELECT ALL 
 offset_amount = 1
 
+# MOVES EACH FRAME BY THE AMOUNT
+move_amount = 0
 # THIS SETS THE STOP FRAME
 end_frame = 40
 
@@ -47,3 +49,8 @@ while True:
     if 'CANCELLED' in ret:
         break
     
+if move_amount != 0:
+    bpy.ops.transform.transform(context_override, mode='TIME_TRANSLATE', value=(move_amount, 0, 0, 0), orient_axis='Z', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)),
+                                orient_matrix_type='GLOBAL', mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size = move_amount,
+                                use_proportional_connected=False, use_proportional_projected=False)
+
